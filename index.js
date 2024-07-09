363123f0340f302d3f4cff6eb7ab0450b9e69ddb
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-09 12:41:25 +0600

    delete operation added

--- index.js
+++ index.js
@@ -49,9 +49,14 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
 
-  persons = persons.filter((person) => person.id != id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error.message);
+      response.status(400).send({ error: "malformed id" });
+    });
 });
 
 app.post("/api/persons", async (request, response) => {
